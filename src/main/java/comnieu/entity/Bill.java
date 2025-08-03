89e20ba61823d0a7c30ffd522a13b9905915aff0
thinhweb3 <thinhhack1@gmail.com>
2025-08-04 01:10:21 +0700

    Add files via upload

--- src/main/java/comnieu/entity/Bill.java
+++ src/main/java/comnieu/entity/Bill.java
@@ -28,4 +28,14 @@ public class Bill {
     private Integer employeeId;
     private Integer tableId;
     private Integer promotionId;
+    public enum BillStatus {
+    Servicing(0),
+    Completed(1),
+    Cancelled(2);
+
+    private final int value;
+    BillStatus(int value) { this.value = value; }
+    public int getValue() { return value; }
+}
+
 }
