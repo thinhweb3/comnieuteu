528ae7025a231e95bec0e08b3d3f814731e72f71
thinhweb3 <thinhhack1@gmail.com>
2025-08-03 18:37:31 +0700

    Add files via upload

--- src/main/java/comnieu/dao/ImportReceiptDAO.java
+++ src/main/java/comnieu/dao/ImportReceiptDAO.java
@@ -1,6 +1,8 @@
 package comnieu.dao;
 
 import comnieu.entity.ImportReceipt;
+import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -10,4 +12,6 @@ import java.util.List;
 public interface ImportReceiptDAO extends CrudDAO<ImportReceipt, Long> {
     List<ImportReceipt> findBySupplierId(Integer supplierId);
     List<ImportReceipt> findByEmployeeId(Integer employeeId);
+    List<ImportReceipt> findByDateRange(LocalDate from, LocalDate to);
+
 }
---
+++ src/main/java/comnieu/dao/ImportReceiptDAOImpl.java
@@ -0,0 +1,93 @@
+package comnieu.dao.impl;
+
+import comnieu.dao.ImportReceiptDAO;
+import comnieu.entity.ImportReceipt;
+import comnieu.util.XJdbc;
+import comnieu.util.XQuery;
+import java.time.LocalDate;
+import java.util.Date;
+
+import java.util.List;
+
+/**
+ * DAO implementation for ImportReceipt
+ * 
+ * @author Admin
+ */
+public class ImportReceiptDAOImpl implements ImportReceiptDAO {
+    final String findByDateRangeSql = "SELECT * FROM ImportReceipt WHERE CONVERT(DATE, ImportDate) BETWEEN ? AND ?";
+
+    final String insertSql = """
+        INSERT INTO ImportReceipt (ImportDate, SupplierId, EmployeeId)
+        VALUES (?, ?, ?)
+    """;
+
+    final String updateSql = """
+        UPDATE ImportReceipt
+        SET ImportDate = ?, SupplierId = ?, EmployeeId = ?
+        WHERE Id = ?
+    """;
+
+    final String deleteSql = "DELETE FROM ImportReceipt WHERE Id = ?";
+    final String findAllSql = "SELECT * FROM ImportReceipt";
+    final String findByIdSql = "SELECT * FROM ImportReceipt WHERE Id = ?";
+    final String findBySupplierIdSql = "SELECT * FROM ImportReceipt WHERE SupplierId = ?";
+    final String findByEmployeeIdSql = "SELECT * FROM ImportReceipt WHERE EmployeeId = ?";
+
+    @Override
+    public ImportReceipt create(ImportReceipt entity) {
+        Object[] values = {
+            entity.getImportDate(),
+            entity.getSupplierId(),
+            entity.getEmployeeId()
+        };
+        long id = XJdbc.executeInsert(insertSql, values);
+        entity.setId(id);
+        return entity;
+    }
+
+    @Override
+    public void update(ImportReceipt entity) {
+        Object[] values = {
+            entity.getImportDate(),
+            entity.getSupplierId(),
+            entity.getEmployeeId(),
+            entity.getId()
+        };
+        XJdbc.executeUpdate(updateSql, values);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        XJdbc.executeUpdate(deleteSql, id);
+    }
+
+    @Override
+    public List<ImportReceipt> findAll() {
+        return XQuery.getBeanList(ImportReceipt.class, findAllSql);
+    }
+
+    @Override
+    public ImportReceipt findById(Long id) {
+        return XQuery.getSingleBean(ImportReceipt.class, findByIdSql, id);
+    }
+
+    @Override
+    public List<ImportReceipt> findBySupplierId(Integer supplierId) {
+        return XQuery.getBeanList(ImportReceipt.class, findBySupplierIdSql, supplierId);
+    }
+
+    @Override
+    public List<ImportReceipt> findByEmployeeId(Integer employeeId) {
+        return XQuery.getBeanList(ImportReceipt.class, findByEmployeeIdSql, employeeId);
+    }
+@Override
+public List<ImportReceipt> findByDateRange(LocalDate from, LocalDate to) {
+    final String sql = "SELECT * FROM ImportReceipt WHERE ImportDate BETWEEN ? AND ?";
+    Date sqlFrom = java.sql.Date.valueOf(from);
+    Date sqlTo   = java.sql.Date.valueOf(to);
+    return XQuery.getBeanList(ImportReceipt.class, sql, sqlFrom, sqlTo);
+}
+
+
+}
